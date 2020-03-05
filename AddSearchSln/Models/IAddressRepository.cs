da97b92c6b4699fee6ab169bd3a6a58c0d7e1d76
Harshal Vadnere <harshal10@gmail.com>
2020-03-04 18:48:50 -0800

    Add mock repo and interface

---
+++ AddSearchSln/Models/IAddressRepository.cs
@@ -0,0 +1,14 @@
+﻿using AddressSearchAlpha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddSearchSln.Models
+{
+    interface IAddressRepository
+    {
+        AddressModel GetAddressCountry(String id);
+        
+    }
+}
---
+++ AddSearchSln/Models/MockRepo.cs
@@ -0,0 +1,31 @@
+﻿using AddressSearchAlpha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddSearchSln.Models
+{
+    public class MockRepo : IAddressRepository
+    {
+        private List<AddressModel> _AddressList;
+        public MockRepo()
+        {
+            _AddressList = new List<AddressModel>()
+            {
+                new AddressModel { Country = "United States of America", Id = "US"},
+                new AddressModel { Country = "Uruguay", Id = "UY" },
+                new AddressModel { Country = "Somalia", Id = "SO" },
+                new AddressModel { Country = "Portugal", Id = "PT" },
+                new AddressModel { Country = "Netherlands", Id = "NL" },
+                new AddressModel { Country = "India", Id = "IN" }
+            };
+        }
+
+        public AddressModel GetAddressCountry(String id)
+        {
+            return _AddressList.FirstOrDefault(a => a.Id == id);
+        }
+
+    }
+}
