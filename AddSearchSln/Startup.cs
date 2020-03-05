f36cb314bce7d449431192ad8cf3431d6d4e210a
NishigandhaMhatre <mhatrenishig@seattleu.edu>
2020-03-04 16:13:28 -0800

    Fixed the bug with View

--- AddSearchSln/Startup.cs
+++ AddSearchSln/Startup.cs
@@ -26,6 +26,7 @@ namespace AddSearchSln
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             // requires using Microsoft.Extensions.Options
             services.Configure<AddressDatabaseSettings>(
                 Configuration.GetSection(nameof(AddressDatabaseSettings)));
