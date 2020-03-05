f820b9b57a5f3af2b09102d31b1aab2c42fe1fee
NishigandhaMhatre <mhatrenishig@seattleu.edu>
2020-03-04 17:11:47 -0800

    Added Json property

--- AddSearchSln/Models/AddressModel.cs
+++ AddSearchSln/Models/AddressModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-
+using Newtonsoft.Json;
 
 namespace AddressSearchAlpha.Models
 {
@@ -15,6 +15,7 @@ namespace AddressSearchAlpha.Models
         public string Id { get; set; }
 
         [BsonElement("Name")]
+        [JsonProperty("Name")]
         public string Country { get; set; }
     }
 }
