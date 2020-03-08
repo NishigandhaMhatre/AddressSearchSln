9593fdd034bd0aaa73d7ec091b226947477632fa
Harshal Vadnere <harshal10@gmail.com>
2020-03-07 22:40:46 -0800

    Code cleanup.

--- AddSearchSln/Startup.cs
+++ AddSearchSln/Startup.cs
@@ -28,8 +28,6 @@ namespace AddSearchSln
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
